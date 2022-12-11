a38dc6a99af76afcd8af1ee204344d6a1f8723de
brodie02 <marshallbrodie0@gmail.com>
2022-12-11 11:42:10 +1030

    Add homepage get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const { User, Post } = require('../models')
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 router.get('/login', (req, res) => {
-    res.render('login')
+  res.render('login')
 })
 
 router.get('/signup', (req, res) => {
