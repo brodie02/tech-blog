b906719ff816c9e479b12abcd327a6dbcf542358
brodie02 <marshallbrodie0@gmail.com>
2022-12-11 13:56:31 +1030

    Add accessibility based on if the user is logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 const { User, Post } = require('../models')
 
 router.get('/', async (req, res) => {
@@ -15,7 +16,8 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({ plain: true }));
 
         res.render('homepage', {
-            posts
+            posts,
+            logged_in: req.session.logged_in
         })
     } catch (error) {
         res.status(500).json(error)
@@ -23,15 +25,39 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/login', (req, res) => {
-  res.render('login')
+    try {
+        if (req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
+        
+        res.render('login');
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 router.get('/signup', (req, res) => {
-    res.render('signup')
+    try {
+        if (req.session.logged_in) {
+            res.redirect('/');
+            return;
+        }
+        
+        res.render('signup');
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', withAuth, (req, res) => {
+    try {
+        res.render('dashboard', {
+            logged_in: true
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
