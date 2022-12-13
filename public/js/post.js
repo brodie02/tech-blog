b5b533db8af34ad365dbf7371ad69c788eee982a
brodie02 <marshallbrodie0@gmail.com>
2022-12-13 20:51:06 +1030

    Post posts to database

---
+++ public/js/post.js
@@ -0,0 +1,23 @@
+const newPost = async (event) => {
+    event.preventDefault();
+    console.log('bbrbr');
+
+    const title = document.querySelector('#title-input').value.trim();
+    const content = document.querySelector('#content').value.trim();
+
+    if (title && content) {
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        console.log(response.statusText);
+      }
+    }
+};
+
+document.getElementById('create-post').addEventListener('click', newPost)
