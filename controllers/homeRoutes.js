bdd71ceecdf507110d54117809dab12dd821268b
brodie02 <marshallbrodie0@gmail.com>
2022-12-13 20:32:54 +1030

    Fix modal bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,6 +24,25 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {
+            posts,
+            logged_in: req.session.logged_in
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 router.get('/login', (req, res) => {
     try {
         if (req.session.logged_in) {
@@ -45,16 +64,6 @@ router.get('/signup', (req, res) => {
         }
         
         res.render('signup');
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
-
-router.get('/dashboard', withAuth, (req, res) => {
-    try {
-        res.render('dashboard', {
-            logged_in: true
-        })
     } catch (error) {
         res.status(500).json(error)
     }
