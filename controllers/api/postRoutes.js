4a3220e0ec7a5c9111e67bf97b73754fd9d95351
brodie02 <marshallbrodie0@gmail.com>
2022-12-20 20:19:41 +1030

    Add update post function

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -35,5 +35,30 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+
+    if (!postData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 module.exports = router
 
---
+++ public/js/updatePost.js
@@ -0,0 +1,26 @@
+const updatePost = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input').value.trim();
+    const content = document.querySelector('#content').value.trim();
+
+    const urlString = window.location.href
+    const urlParameter = urlString.split('/')
+    const post_id = urlParameter[4]
+
+    if (title && content) {
+      const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        console.log(response.statusText);
+      }
+    }
+};
+
+document.querySelector('#update').addEventListener('click', updatePost)
